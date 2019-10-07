171e60516a93bf3caded497194af2732351d8aee
lea <lea.ong@dauphine.eu>
2019-10-07 15:19:50 +0200

    Td01 exo 1 et 2

---
+++ shapes/src/main/java/fr/dauphine/ja/onglea/shapes/LigneBrisee.java
@@ -0,0 +1,18 @@
+package fr.dauphine.ja.onglea.shapes;
+
+public class LigneBrisee {
+	
+	private Point [] listPoints;
+	private int nbMaxPoint;
+	
+	public LigneBrisee(int nbMaxPoint) {
+		this.listPoints =new Point [nbMaxPoint];
+		this.nbMaxPoint=nbMaxPoint;
+	
+	}
+	
+	public void add (Point p) {
+		
+	}
+	
+}
